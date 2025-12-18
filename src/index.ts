0639f9eb393861339ea82f093362e2f47d60d887
Brian E. Granger <ellisonbg@gmail.com>
2025-12-18 13:14:00 -0700

    Update icon.

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { BraketDevicesWidget } from './widget';
  */
 const borderClearIcon = new LabIcon({
   name: 'braket:border-clear',
-  svgstr: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M7 5h10v2h2V3c0-1.1-.9-2-2-2H7c-1.1 0-2 .9-2 2v4h2zm0 14h2v-2H7zm12-2h-2v2h2c1.1 0 2-.9 2-2v-4h-2zm0-12h2v2h-2zM5 7H3v2h2zm0 4H3v2h2zm0 4H3v2h2zm12 0h2v2h-2zM9 19v2h2v-2zm4 0v2h2v-2z"/></svg>`
+  svgstr: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M7 5h2V3H7zm0 8h2v-2H7zm0 8h2v-2H7zm4-4h2v-2h-2zm0 4h2v-2h-2zm-8 0h2v-2H3zm0-4h2v-2H3zm0-4h2v-2H3zm0-4h2V7H3zm0-4h2V3H3zm8 8h2v-2h-2zm8 4h2v-2h-2zm0-4h2v-2h-2zm0 8h2v-2h-2zm0-12h2V7h-2zm-8 0h2V7h-2zm8-6v2h2V3zm-8 2h2V3h-2zm4 16h2v-2h-2zm0-8h2v-2h-2zm0-8h2V3h-2z"/></svg>`
 });
 
 /**
